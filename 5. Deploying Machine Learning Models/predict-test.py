bf9719aca6c41c52d2c4b1386a40654e83c8d7fe
Ugur Orhan <52375711+ukvar@users.noreply.github.com>
2022-10-10 21:13:18 +0300

    Add files via upload

---
+++ 5. Deploying Machine Learning Models/predict-test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'xyz-123'
+customer = {
+    "reports": 0,
+     "share": 0.245,
+      "expenditure": 3.438,
+       "owner": "yes"
+       }
+
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+if response['card'] == True:
+    print('sending promo email to %s' % customer_id)
+else:
+    print('not sending promo email to %s' % customer_id)
